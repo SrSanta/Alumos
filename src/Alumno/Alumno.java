cf3803d4ff416ad95e04d14fa55c27e2ccacd001
SrSanta <93655642+SrSanta@users.noreply.github.com>
2023-02-15 18:13:22 +0100

    Update Alumno.java

--- src/Alumno/Alumno.java
+++ src/Alumno/Alumno.java
@@ -8,34 +8,7 @@ package Alumno;
 
 public class Alumno {
 
-    /**
-     * @return the nombre
-     */
-    public String getNombre() {
-        return nombre;
-    }
-
-    /**
-     * @param nombre the nombre to set
-     */
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    /**
-     * @return the nota
-     */
-    public double getNota() {
-        return nota;
-    }
-
-    /**
-     * @param nota the nota to set
-     */
-    public void setNota(double nota) {
-        this.nota = nota;
-    }
-  private String nombre;
-  private double nota = 0.0;
+  public String nombre;
+  public double nota = 0.0;
   
 }
