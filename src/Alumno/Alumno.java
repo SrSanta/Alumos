8e7213ace417333a401a2c356aeecb0d81f55bb9
SrSanta <andrescondegarcia11@gmail.com>
2023-02-15 17:37:10 +0100

    creacion de get y set de las variables

--- src/Alumno/Alumno.java
+++ src/Alumno/Alumno.java
@@ -3,11 +3,39 @@ package Alumno;
 /**
  * Alumno.java
  * Definición de la clase Alumno
- * @author Luis José Sánchez
+ * @author Andres Conde Garcia
  */
 
 public class Alumno {
-  public String nombre;
-  public double notaMedia = 0.0;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the nota
+     */
+    public double getNota() {
+        return nota;
+    }
+
+    /**
+     * @param nota the nota to set
+     */
+    public void setNota(double nota) {
+        this.nota = nota;
+    }
+  private String nombre;
+  private double nota = 0.0;
   
 }
